58c77d2227cf13eaa168026860d679a8ce159374
unknown <viniciussieben7@gmail.com>
2023-04-14 13:18:44 -0300

    Adicionado aula Introducao a POO

---
+++ IntroducaoPOO/src/Conta.java
@@ -0,0 +1,8 @@
+
+public class Conta {
+	public String nomeCliente;
+	public int numeroAgencia;
+	public int numeroConta;
+	public float limite;
+	public float saldo;
+}
---
+++ IntroducaoPOO/src/Principal.java
@@ -0,0 +1,9 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Conta conta;
+		conta = new Conta(); // Instanciando um novo objeto
+	}
+
+}
